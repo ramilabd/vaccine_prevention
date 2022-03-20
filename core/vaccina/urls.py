8f62442b512dac591838c2b702c254e024cf9903
ramilabd <ramsmart@mail.ru>
2022-03-20 17:33:08 +0500

    feat: display employee information by url /employees

--- core/vaccina/urls.py
+++ core/vaccina/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ViewMainPage
+from .views import ViewMainPage, ListEmployee
 
 
 urlpatterns = [
     path('', ViewMainPage.as_view(), name='index'),
+    path('employees', ListEmployee.as_view(), name='employees'),
 ]
--- core/vaccina/views.py
+++ core/vaccina/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from django.views import View
+from .models import Employee
 
 
 class ViewMainPage(View):
     def get(self, request, *args, **kwargs):
         return render(request, 'vaccina_page/index.html')
+
+
+class ListEmployee(View):
+    def get(self, request, *args, **kwargs):
+        employees = Employee.objects.all()
+        return render(request, 'vaccina_page/employees.html', context={
+            'employees': employees,
+        })
