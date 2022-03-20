096d3e4cffde496d6d9e717368721cbf55ac8aa3
ramilabd <ramsmart@mail.ru>
2022-03-20 17:27:42 +0500

    feat: add metod __str__ to models

--- core/vaccina/models.py
+++ core/vaccina/models.py
@@ -6,13 +6,22 @@ from django.contrib.auth.models import User
 class GroupEmployee(models.Model):
     name_grop = models.CharField('Группа сотрудников', max_length=150)
 
+    def __str__(self):
+        return self.name_grop
+
 
 class Position(models.Model):
     name_post = models.CharField('Должность', max_length=50)
 
+    def __str__(self):
+        return self.name_post
+
 
 class Department(models.Model):
     name_department = models.CharField('Отделение', max_length=50)
+
+    def __str__(self):
+        return self.name_department
 
 
 class Employee(models.Model):
@@ -26,4 +35,11 @@ class Employee(models.Model):
     department = models.ForeignKey(Department, verbose_name='Отделение', on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.first_name, self.patronymic, self.last_name
+        return '{0} {1} {2}, дата рождения: {3}, возраст {4} лет, должность {5}'.format(
+            self.first_name,
+            self.patronymic,
+            self.last_name,
+            self.date_of_birth,
+            self.age,
+            self.post,
+        )
